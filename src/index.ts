b4cddab6c1e959fccc63c1b4bdac19d01111c0a0
Umma Ahimsha <uahimsha@gmail.com>
2020-12-08 18:36:12 +0700

    added collection view

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
-import { UserEdit } from "./views/UserEdit";
-import { User } from "./models/User";
+/*  Option to show collection of users  */
 
-const itachi = User.buildUser({ name: "Itachi", age: 23 });
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const root = document.getElementById("root");
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-if (root) {
-  const userEdit = new UserEdit(root, itachi);
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-  userEdit.render();
-} else {
-  throw new Error("Root element not found");
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
+
+/*  Option to show single user and edit */
+
+// import { UserEdit } from "./views/UserEdit";
+// import { User } from "./models/User";
+
+// const itachi = User.buildUser({ name: "Itachi", age: 23 });
+
+// const root = document.getElementById("root");
+
+// if (root) {
+//   const userEdit = new UserEdit(root, itachi);
+
+//   userEdit.render();
+// } else {
+//   throw new Error("Root element not found");
+// }
